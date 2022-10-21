99a901eba4673ccf4b3162ca405d84257d477b8d
Arango155 <jdarango031@gmail.com>
2022-10-21 13:35:08 -0600

    Realizo una ventana modal para el formulario proyecto

--- web/script.js
+++ web/script.js
@@ -1,1 +1,15 @@
 
+const btnAbrirModal=
+document.querySelector("#btn-abrir-modal");
+const btnCerrarModal =
+document.querySelector("#btn-cerrar-modal");
+const modal =
+document.querySelector("#modal");
+
+btnAbrirModal.addEventListener("click",()=>{
+    modal.show();
+})
+
+btnCerrarModal.addEventListener("click",()=>{
+modal.close();
+})
