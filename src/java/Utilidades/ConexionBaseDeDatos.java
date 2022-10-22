6efe39fa674093da226c28026c1f601544641697
pablosalazarbr <redpablo16@gmail.com>
2022-10-22 00:06:11 -0600

    Se agregan modulos sql

--- src/java/Utilidades/ConexionBaseDeDatos.java
+++ src/java/Utilidades/ConexionBaseDeDatos.java
@@ -11,24 +11,25 @@ import java.sql.SQLException;
 
 /**
  *
- * @author JP
+ * @author Pablosalazarbr
  */
 public class ConexionBaseDeDatos {
-     private String url="jdbc:mysql://127.0.0.1:3306/universidad";//url de MySQL
-    private String usuario="root";// usuario de mysql local
-    private String clave="root"; 
-    private Connection conexion=null;  
+    private String url="jdbc:mysql://127.0.0.1:3306/universidad";//url de MySQL
+    private String usuario="root"; // usuario de mysql local
+    private String clave="root"; //Password del usuario mysql
+    private Connection conexion=null; //Seteamos una variable para validar el estado de conexion, inicialmente null
     
+    //Funcion para intentar una conexion a la bd
     public Connection conectar(){   
         try{
-            Class.forName("com.mysql.jdbc.Driver");
-            conexion=DriverManager.getConnection( url, usuario,clave);
+            Class.forName("com.mysql.jdbc.Driver"); //Obtenemos el drivermanager para conexion
+            conexion=DriverManager.getConnection( url, usuario,clave); //Ejecutamos una conexion a la bd
         }  catch (SQLException ex) {
-            ex.printStackTrace();
+            ex.printStackTrace(); //Si existiera un error en la ejecucion sql lo almacenamos
         } catch (ClassNotFoundException ex) {   
-           ex.printStackTrace();
+           ex.printStackTrace(); //Si no encontraramos la clase para la conexion almacenamos el error obtenido
         }         
-        return conexion;        
+        return conexion; //Devolvemos el estado de la conexion     
     }
     
 }
