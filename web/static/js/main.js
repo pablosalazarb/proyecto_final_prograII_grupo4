e87266abb30dbc8cf77848d622ba38bc07fba99b
pablosalazarbr <redpablo16@gmail.com>
2022-10-22 00:23:23 -0600

    Se modifica ruta de archivo main.js

---
+++ web/static/js/main.js
@@ -0,0 +1,23 @@
+/*
+Universidad Mariano Galvez de Guatemala
+Archivos de controlador para el frontend implementando JS
+Proyecto Final de Programacion II
+Grupo 4
+Autores: Suzzane Acevedo, Josue Arango, Brayan Martinez, Melvin Henrandez, Juan P. Salazar
+Fecha de creacion: 22/10/2022
+*/
+
+
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+
+}
+
+document.addEventListener('keydown', function(event){
+	if(event.key === "Escape"){
+document.getElementById("popup-1").classList.toggle("active");
+	}
+})
+
+
+
