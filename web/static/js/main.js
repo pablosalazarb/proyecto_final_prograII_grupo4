122cd9e2bea74587461ebce4403a18a62ba8821f
pablosalazarbr <redpablo16@gmail.com>
2022-10-22 01:10:22 -0600

    Se corrigen rutas de css

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -39,3 +39,29 @@ function togglePopup2(){
 }
 
 
+/*
+
+-------------------- INICIAN CONTROLADORES DEL LOGIN --------------------
+
+*/
+
+function login(){
+    var user, pass;
+    user = document.getElementById("user").value;
+    pass = document.getElementById("pass").value;
+    if(user=="Suzzanne" && pass== "Holi"){
+        window.location="index.html";
+    }else{
+        errMsj('Invalid username or password');
+    }
+    
+}
+
+function errMsj(msj){
+  Swal.fire({
+  icon: 'error',
+  title: msj,
+  showConfirmButton: false,
+  timer: 1500
+})
+}
