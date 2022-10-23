9d297eb859dc8e82efeacb56633cd6682acc39d1
Arango155 <jdarango031@gmail.com>
2022-10-23 12:03:10 -0600

    Arreglo el js para el index login

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -71,3 +71,33 @@ function errMsj(msj){
   timer: 1500
 })
 }
+function comeDom (){
+    let usuarios = new Object();
+    let user=document.getElementById("user");
+    let pass=document.getElementById("pass");
+    let tipUsr=document.getElementById("tipUsr");
+    usuarios.user=user.value;
+    usuarios.pass=pass.value;
+    usuarios.tipUsr=tipUsr.value;
+}
+
+function sendData2(){
+	const XHR = new XMLHttpRequest();
+	  var formData = new URLSearchParams(new FormData(document.getElementById('formuser'))).toString();
+	  XHR.addEventListener('error',(event) => {alert('Oops! ah ocurrido un error (T_T)');});
+	  XHR.open('POST', 'ServletUser', true);
+          XHR.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                    
+          XHR.onload = () => { if (XHR.readyState === XHR.DONE && XHR.status === 200){
+            console.log("response => " + XHR.response);
+            susMsj('Registro Exitoso'); }};
+          XHR.send(formData);	
+}
+function susMsj(msj){
+  Swal.fire({
+  icon: 'success',
+  title: msj,
+  showConfirmButton: false,
+  timer: 1500
+})
+}
